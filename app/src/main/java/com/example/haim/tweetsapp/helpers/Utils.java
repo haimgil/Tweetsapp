86aed85e4f410a7f1a919cf24e84c5a5aac3f590
haimgil <haim.gilboa10@gmail.com>
2015-03-16 12:58:17 +0200

    add icons

---
+++ app/src/main/java/com/example/haim/tweetsapp/helpers/Utils.java
@@ -0,0 +1,24 @@
+package com.example.haim.tweetsapp.helpers;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by Haim on 12/27/2014.
+ */
+public class Utils {
+
+    public static void alert(Context ctx, String title, String msg){
+        AlertDialog alertDialog = new AlertDialog.Builder(ctx).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(msg);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+}
