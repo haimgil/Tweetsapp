757230dabf0b1de0383e726b5b46b68c60f333b8
haimgil <haim.gilboa10@gmail.com>
2015-04-10 09:38:23 +0300

    Add variables to Message class for set and calculates average_rating.

--- app/src/main/java/com/example/haim/tweetsapp/Objcets/Message.java
+++ app/src/main/java/com/example/haim/tweetsapp/Objcets/Message.java
@@ -5,12 +5,28 @@ package com.example.haim.tweetsapp.Objcets;
  */
 public class Message {
 
-    private String msgText;
+    private String msg_text;
     private String time;
+    private int rating;
+    private double average_rating;
+    private int number_of_ratings;
 
-
-    public Message(String msgText, String time) {
-        this.msgText = msgText;
+    public Message(String msg_text, String time) {
+        this.msg_text = msg_text;
         this.time = time;
+        rating = 0;
+        average_rating = 0;
+        number_of_ratings = 0;
+    }
+
+    public double getAverage_rating() {
+        return average_rating;
+    }
+
+    public void setAverage_rating(double userRate) {
+        this.rating += userRate;
+        this.number_of_ratings++;
+        double tmpAverage = this.rating / this.number_of_ratings;
+        this.average_rating = tmpAverage;
     }
 }
